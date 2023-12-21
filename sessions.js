ec1a0ea33dc18a7123666abcdffe625fb6495212
ahkjeflhkeuvha <w2317@e-mirim.hs.kr>
2023-12-21 10:16:47 +0900

    💄 Login - 2

--- sessions.js
+++ sessions.js
@@ -1,12 +1,27 @@
 var express = require('express')
 var session = require('express-session')
+var bodyParser = require('body-parser')
 var app = express()
 
+app.use(bodyParser.urlencoded({extended : false}))
 app.use(session({
     secret: 'randomkey', 
     resave: false,
     saveUnitionaized: true //session id를 사용하기 전까지 접속 X
 }))
+
+app.post('/auth/login', (req, res)=>{
+    var savedUser = {
+        user : 'jieun',
+        pw : '1234'
+    }
+    var user = req.body.username
+    var pw = req.body.password
+    
+    
+    if(user === savedUser.user && pw === savedUser.pw) res.redirect('/welcome')
+    else res.send(`who are you <a href="/auth/login>login</a>"`)
+})
 
 app.get('/auth/login', (req, res)=>{
     var output = `
